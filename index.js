043ec616af13d56895d22c68bb1af4804cb65461
Abdulaziz <muhtarovabdulaziz39@gmail.com>
2025-12-14 19:28:08 +0500

    homework-sikl

---
+++ index.js
@@ -0,0 +1,131 @@
+// var age = +prompt("Yoshingiz nechida")
+// console.log(age);
+// console.log(typeof age);
+// if (age >= 18 && age <= 59) {
+//     alert("unda siz xali ishlashingiz kerak")
+// }else if (isNaN(age)) {
+//     alert("Iltimos yoshingizni tog'ri kiriting,son yozing !")
+// 
+// else if (age > 59) {
+//     alert("pensiyaga chiqish voqti boldi")
+// }else {
+//     alert("ishlash xali voxli, borib o'king")
+// }
+
+// oylar
+
+// var moth = +prompt("Qaysi oyda tug'ilgansiz? 1 dan 12 gacha son kiriting !") 
+// if (moth === 1 || moth === 2|| moth === 12) {
+//     alert("Siz qishda tug'ilgansiz !")
+// }else if (moth >= 3 && moth <= 5){
+//     alert("Siz bahorda tug'ilgansiz !")
+// }else if (moth >= 6 && moth <= 8){
+//     alert("Siz yozdaa tug'ilgansiz !")
+// }else if (moth >= 9 && moth <= 11) {
+//     alert("Siz luzda tug'ilgansiz !")
+// }else {
+//     alert("Iltimos 1 dan 12 gacha son kiriting")
+// }
+
+// var porol = +prompt("Iltimos porol kiring")
+// var porol2 = +prompt("Iltimos porolingizni takroran kiriting !")
+// parol2.length === 123456
+// if (porol === porol2 && porol.length >5) {
+//     alert("Xush kelibsiz")
+// } else {
+//     alert("Uzunligi yoki parol notog'ri")
+// }
+
+
+// var yosh = +prompt("Yoshing nechida")
+// var guvohnomaYili = +prompt("Guvohnoma olingan yili")
+// var texnik = prompt("Texnik korikdan otkanmi ?")
+// if (yosh >= 18 && guvohnomaYili > 0 && texnik === "ha") {
+//     alert("Mashina haydashingiz mumkin")
+// }
+// else {
+//     alert("Siz moshina haydashingiz mumkin emas !")
+// }
+
+
+// Theme Function and randomayzer !!!  13/12/2025.
+
+// var i = 0   //вместо i можно написать что угодно то-есть названия !!!
+// while (i <= 10) {
+//     document.write(i + "<br>")
+//     i = i + 1
+//     // i++
+// }
+
+//Функция умножения !!!
+
+// var num = prompt("Raqam yoz")
+// var power = prompt("Daraja yoz")
+// var res = 1;
+// var i = 1
+
+// while (i <=  power) {
+//     res = power * num 
+//     i++
+// }
+// alert(res)
+
+// Функция выводит цисло от 1 до 10 подряд !!!
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i + "<br>")
+// }
+
+// Это функция пропускает одну цифру которую выделили !!!
+
+// for (var i = 1; i <= 10; i++) {
+//     if (i === 5){ //В этом случае этот цифра <5> !!!
+//         continue
+//     }
+//     document.write(i + "<br>")
+// }
+
+
+// <i> yozish shart emas chunki u ozgaruvchi lyuboy so'z yozsa bo'ladi !!!
+
+//Это функция для вычисления не четных чисел !!! 
+
+// for (var i = 0; i <= 100; i++) {
+//     if (i % 2 != 0) { 
+//         document.write(i + '<br>')
+//     }
+// }
+
+//Это функция для вычисления четных чисел !!!
+
+// for (var i = 2; i <= 100; i++){
+//     if (i % 2 === 0){
+//         document.write(i + '<br>')
+//     }
+// }
+
+
+
+
+
+
+
+
+// Homework
+//Binchi vazifa !!!
+// for (var car = 1; car <= 7; car++) {
+//     var car = +prompt("Qaysi 'car' dasiz?, iltimos son kirgizing (1-7 gacha son kiriting!)")
+//     var time = +prompt("Qaysi voqtdasiz (0 - 23 gacha son kiriting!)")
+//     var pass = prompt("Guvohnomangiz bormi? (ha yoki yoq)!")
+//     if (car >= 1 && car <= 7 && time >= 8 && time <= 20 && pass === "ha") {
+//         alert('Siz kirishingiz mumkin :)')
+//     } else {
+//         alert('Siz kirishingiz mumkin emas!!!')
+//     }
+// }
+
+//Ikinchi vazifa !!!
+// for (var i = 1; i <= 100; i++) {
+
+// }
+
